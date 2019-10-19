9462d2171a9c545fb01620a2bbdc43d0d3b6bc38
storrellas <storrellas@gmail.com>
2019-10-19 04:37:10 +0000

    ENH: Adding user/info

--- test/main.py
+++ test/main.py
@@ -1,18 +1,29 @@
 import jwt
 import datetime
 import time
-jwt_payload = jwt.encode({
-    'exp': int(time.time()) + 2
-}, 'secret')
+import pymongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-time.sleep(3)
-try:
-  # JWT payload is now expired
-  # But with some leeway, it will still validate
-  token = jwt.decode(jwt_payload, 'secret', leeway=10, algorithms=['HS256'])
-  print(token)
-  print(time.time() > token['exp'])
 
-except jwt.ExpiredSignatureError:
-  # Signature has expired
-  print("Expired!!!")
+# jwt_payload = jwt.encode({
+#     'exp': int(time.time()) + 2
+# }, 'secret')
+
+# time.sleep(3)
+# try:
+#   # JWT payload is now expired
+#   # But with some leeway, it will still validate
+#   token = jwt.decode(jwt_payload, 'secret', leeway=10, algorithms=['HS256'])
+#   print(token)
+#   print(time.time() > token['exp'])
+
+# except jwt.ExpiredSignatureError:
+#   # Signature has expired
+#   print("Expired!!!")
+
+# Create MongoClient
+client = MongoClient('localhost', 27017)
+db = client.asdt
+user = db.users.find_one({'_id': ObjectId('5da995c4f67b2d2afcec0c64')})
+print(user)
--- user/views.py
+++ user/views.py
@@ -4,6 +4,9 @@ import time
 
 # pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
 
 # Django import
 from django.shortcuts import render
@@ -28,8 +31,10 @@ client = MongoClient('localhost', 27017)
 db = client.asdt
 
 class ASDTUser:
-  username = 'sergi'
-  password = 'torrellas'
+  data = {}
+  def __init__(self, data):
+    self.data = data
+
   def is_authenticated(self):
     return True
 
@@ -41,15 +46,13 @@ class ASDTAuthentication(authentication.BaseAuthentication):
       encoded_jwt = authorization_header.split(' ')[1]
       try:
         decoded_jwt = jwt.decode(encoded_jwt, settings.SECRET_KEY, algorithms=['HS256'])
-
-        user = db.users.find_one({'id':decoded_jwt['id']})
-        logger.info("Sergi")
-        print(user)
-        print(decoded_jwt)
-
-
-        return (ASDTUser(), None)
-      except jwt.ExpiredSignatureError:
+        user = db.users.find_one({'_id':ObjectId(decoded_jwt['id'])})
+        return (ASDTUser(data=user), None)
+      except Exception as e:
+        print(e)
+        return (None, None)
+      except jwt.ExpiredSignatureError as e:
+        print("Expired ", str(e))
         return (None, None)
 
       
@@ -96,11 +99,17 @@ class UserInfo(APIView):
     def get(self, request):
         data = {
           'success': True,
-          'data':{
-            'token': '123'
+          'data': {
+            'id': str(request.user.data['_id']),
+            'email': request.user.data['email'],
+            'name': request.user.data['name'],
+            'detectors' : [],
+            'inhibitors' : [],
+            'role' : request.user.data['role'],
           }
-        }
-        print(request.user.username)
+        } 
+        logger.info("In view")
+        print(request.user.data)
         return Response(data)
 
 class AllowedTools(APIView):
