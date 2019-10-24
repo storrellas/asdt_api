b6241e3731a1fac75743e0f607ccc8240acb8287
storrellas <storrellas@gmail.com>
2019-10-24 15:19:25 +0000

    ENH: Removed ununsed code

--- mongo_dummy.py
+++ mongo_dummy.py
@@ -129,13 +129,3 @@ ModelDetector.objects.create(name='Mavic Pro', productId=18, imageType='image/pn
 ModelDetector.objects.create(name='DJI MAVIC PRO DUAL', productId=51, imageType='image/jpeg', image=True, imageCode=1)
 logger.info("Created {}. Done!".format(ModelDetector.objects.all().count()) )
 
-
-
-print( type(ModelDetector.objects.all()) )
-print( ModelDetector.objects.all().fields(id=0).to_json() )
-print( type(ModelDetector.objects.all().fields(id=0).to_json() ) )
-print( ModelDetector.objects.all().fields(id=0) )
-print( type(ModelDetector.objects.all().fields(id=0) ) )
-
-for item in ModelDetector.objects.all().fields(id=0):
-  print(item.to_mongo().to_dict())
