c9bd82ab46f44eaa6d871585900beba24ff270ee
storrellas <storrellas@gmail.com>
2019-10-24 12:24:28 +0000

    ENH: Creating groups

--- mongo_dummy.py
+++ mongo_dummy.py
@@ -12,9 +12,17 @@ from django.conf import settings
 import mongoengine
 mongoengine.connect(settings.MONGO_DB, host=settings.MONGO_HOST, port=int(settings.MONGO_PORT))
 
+print(settings.MONGO_DB)
+print(settings.MONGO_HOST)
+print(settings.MONGO_DB)
 
+# Delete entities
 User.objects.all().delete()
 Group.objects.all().delete()
 Detector.objects.all().delete()
 Inhibitor.objects.all().delete()
 Log.objects.all().delete()
+
+# Groups
+root_group = Group.objects.create(name='ASDT')
+print(root_group)
