dda32d4187fdb12741e702d8c9f6326d4d1c17cc
storrellas <storrellas@gmail.com>
2019-10-25 06:36:21 +0000

    ENH: Created scenario

--- mongo_dummy.py
+++ mongo_dummy.py
@@ -36,7 +36,8 @@ logger.info("Done!")
 # Groups
 ##############
 logger.info("Creating groups ...")
-root_group = Group.objects.create(name='ASDT')
+admin_group = Group.objects.create(name='ADMIN_ASDT')
+viewer_group = Group.objects.create(name='VIEWER_ASDT')
 logger.info("Created {}. Done!".format(Group.objects.all().count()) )
 
 # Users
@@ -56,34 +57,43 @@ display_options = DisplayOptions(mapType='MyMap', zone=[[2,3]],circleZone=[circl
 admin = User.objects.create(email='admin@asdt.eu', name='admin', 
                             password='asdt2019', role='ADMIN',
                             displayOptions=display_options,
-                            group=root_group, hasGroup=True)
+                            group=admin_group, hasGroup=True)
 admin.set_password('asdt2019')
-root_group.users.append(admin)
-root_group.save()
 
 empowered = User.objects.create(email='empowered@asdt.eu', name='empowered', password='asdt2019', role='EMPOWERED')
 empowered.set_password('asdt2019')
-viewer = User.objects.create(email='viewer@asdt.eu', name='viewer', password='asdt2019', role='VIEWER')
+viewer = User.objects.create(email='viewer@asdt.eu', name='viewer', password='asdt2019', role='VIEWER',
+                              group=viewer_group, hasGroup=True)
 viewer.set_password('asdt2019')
 logger.info("Created {}. Done!".format(User.objects.all().count()) )
+
+# Add to groups
+admin_group.users.append(admin)
+admin_group.save()
+viewer_group.users.append(viewer)
+viewer_group.save()
 
 
 # Detectors
 ##############
 logger.info("Creating detectors ...")
-detector = Detector.objects.create(name='moncloa', password='asdt2019', 
+detector1 = Detector.objects.create(name='moncloa', password='asdt2019', 
                         location=DetectorLocation(lat=0, lon=0, height=0))
-root_group.devices.detectors.append(detector)
-root_group.save()
-Detector.objects.create(name='zarzuela', password='asdt2019', 
+detector2 = Detector.objects.create(name='zarzuela', password='asdt2019', 
                         location=DetectorLocation(lat=0, lon=0, height=0))
-Detector.objects.create(name='congreso', password='asdt2019', 
+detector3 = Detector.objects.create(name='congreso', password='asdt2019', 
                         location=DetectorLocation(lat=0, lon=0, height=0))
-Detector.objects.create(name='cuatrovientos', password='asdt2019', 
+detector4 = Detector.objects.create(name='cuatrovientos', password='asdt2019', 
                         location=DetectorLocation(lat=0, lon=0, height=0))
-Detector.objects.create(name='canillas', password='asdt2019', 
+detector5 = Detector.objects.create(name='canillas', password='asdt2019', 
                         location=DetectorLocation(lat=0, lon=0, height=0))
 logger.info("Created {}. Done!".format(Detector.objects.all().count()) )
+
+# Add to groups
+admin_group.devices.detectors.append(detector2)
+admin_group.save()
+viewer_group.devices.detectors.append(detector3)
+viewer_group.save()
 
 # Logs
 ##############
@@ -100,7 +110,7 @@ route = [
 Log.objects.create(dateIni=datetime.datetime.strptime('2019-09-01T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"), 
                     dateFin=datetime.datetime.strptime('2019-09-01T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"),
                     model='ABC', sn='123', productId=1234,
-                    detectors=[detector],
+                    detectors=[detector1, detector2],
                     driverLocation=Location(lat=1.2,lon=3.4), homeLocation=Location(lat=1.2,lon=3.4),
                     maxHeight=12, distanceTravelled=12, distanceToDetector=12,
                     centerPoint=LogCenterPoint(lat=1.0, lon=2.0, aHeight=1.2, fHeight=2.3),
@@ -108,17 +118,27 @@ Log.objects.create(dateIni=datetime.datetime.strptime('2019-09-01T23:00:00.000Z'
 Log.objects.create(dateIni=datetime.datetime.strptime('2019-10-21T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"), 
                     dateFin=datetime.datetime.strptime('2019-10-21T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"),
                   model='ABC', sn='123', productId=1234,
+                  detectors=[detector1, detector2],
+                  driverLocation=Location(lat=1.2,lon=3.4), homeLocation=Location(lat=1.2,lon=3.4),
+                  maxHeight=12, distanceTravelled=12, distanceToDetector=12,
+                  centerPoint=LogCenterPoint(lat=1.0, lon=2.0, aHeight=1.2, fHeight=2.3),
+                  boundingBox=LogBoundingBox(maxLat=1.0, maxLon=2.0, minLat=1.2, minLon=2.3))  
+Log.objects.create(dateIni=datetime.datetime.strptime('2019-10-22T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"), 
+                    dateFin=datetime.datetime.strptime('2019-10-22T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"),
+                  model='ABC', sn='123', productId=1234,
+                  detectors=[detector1, detector3],
                   driverLocation=Location(lat=1.2,lon=3.4), homeLocation=Location(lat=1.2,lon=3.4),
                   maxHeight=12, distanceTravelled=12, distanceToDetector=12,
                   centerPoint=LogCenterPoint(lat=1.0, lon=2.0, aHeight=1.2, fHeight=2.3),
                   boundingBox=LogBoundingBox(maxLat=1.0, maxLon=2.0, minLat=1.2, minLon=2.3))
-Log.objects.create(dateIni=datetime.datetime.strptime('2019-10-22T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"), 
-                    dateFin=datetime.datetime.strptime('2019-10-22T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"),
+Log.objects.create(dateIni=datetime.datetime.strptime('2019-10-24T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"), 
+                    dateFin=datetime.datetime.strptime('2019-10-24T23:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ"),
                   model='ABC', sn='123', productId=1234,
+                  detectors=[detector1, detector2],
                   driverLocation=Location(lat=1.2,lon=3.4), homeLocation=Location(lat=1.2,lon=3.4),
                   maxHeight=12, distanceTravelled=12, distanceToDetector=12,
                   centerPoint=LogCenterPoint(lat=1.0, lon=2.0, aHeight=1.2, fHeight=2.3),
-                  boundingBox=LogBoundingBox(maxLat=1.0, maxLon=2.0, minLat=1.2, minLon=2.3))                                    
+                  boundingBox=LogBoundingBox(maxLat=1.0, maxLon=2.0, minLat=1.2, minLon=2.3))                  
 logger.info("Created {}. Done!".format(Log.objects.all().count()) )
 
 
@@ -132,24 +152,24 @@ ModelDetector.objects.create(name='DJI MAVIC PRO DUAL', productId=51, imageType=
 logger.info("Created {}. Done!".format(ModelDetector.objects.all().count()) )
 
 
-user = User.objects.get(email='admin@asdt.eu')
-user.group
+# user = User.objects.get(email='admin@asdt.eu')
+# user.group
 
 
-queryset = Log.objects.all()
-# Allowed detector list
-detector_list_for_user = []
-for detector in user.group.devices.detectors:
-    detector_list_for_user.append( detector.fetch() )
+# queryset = Log.objects.all()
+# # Allowed detector list
+# detector_list_for_user = []
+# for detector in user.group.devices.detectors:
+#     detector_list_for_user.append( detector.fetch() )
 
-print(len(detector_list_for_user) )
-for detector in detector_list_for_user:
-  print(detector)
-  print(detector.to_mongo().to_dict())
+# print(len(detector_list_for_user) )
+# for detector in detector_list_for_user:
+#   print(detector)
+#   print(detector.to_mongo().to_dict())
 
-#queryset = queryset.filter(detectors__in=[detector_list_for_user])
-for item in queryset:
-  print(item.detectors[0])
-  print(item.detectors[0].to_json())
-  print(item.detectors[0].as_pymongo())
-  print(item.detectors[0].to_mongo())
+# #queryset = queryset.filter(detectors__in=[detector_list_for_user])
+# for item in queryset:
+#   print(item.detectors[0])
+#   print(item.detectors[0].to_json())
+#   print(item.detectors[0].as_pymongo())
+#   print(item.detectors[0].to_mongo())
