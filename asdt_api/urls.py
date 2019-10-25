d1589b9a01fbce68395dea754821fb38f66a0715
storrellas <storrellas@gmail.com>
2019-10-25 13:35:48 +0000

    ENH: Changed routes

--- asdt_api/urls.py
+++ asdt_api/urls.py
@@ -31,14 +31,15 @@ from logs.views import LogByPage
 #     path('model/', include('modeldetectors.urls')),
 # ]
 
+PREFIX = 'api/v2/'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('logs', LogByPage.as_view()),
-    path('logs/', include('logs.urls')),
-    path('model', ModelDetectorView.as_view()),
-    path('models/', include('modeldetectors.urls')),
-    path('model/', include('modeldetectors.urls')),
+    path(PREFIX + 'admin/', admin.site.urls),
+    path(PREFIX + 'user/', include('user.urls')),
+    path(PREFIX + 'logs', LogByPage.as_view()),
+    path(PREFIX + 'logs/', include('logs.urls')),
+    path(PREFIX + 'model', ModelDetectorView.as_view()),
+    path(PREFIX + 'models/', include('modeldetectors.urls')),
+    path(PREFIX + 'model/', include('modeldetectors.urls')),
 ]
 
