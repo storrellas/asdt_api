dc599437e78af552a909c5e789562b343199a475
storrellas <storrellas@gmail.com>
2019-10-29 11:13:00 +0000

    ENH: Migrating to use viewsets

--- logs/urls.py
+++ logs/urls.py
@@ -2,12 +2,19 @@
 from django.conf.urls import url
 from django.urls import path, include
 
+# DRF inludes
+from rest_framework import routers
 
 # Project imports
 from .views import *
 
-urlpatterns = [    
-    path('<str:log_id>/', LogByIdView.as_view()),
-    url(r'^$', LogByPageView.as_view()) 
-]
+# Do not change the order as URL resolution is done in order
+router = routers.DefaultRouter()
+router.register(r'', LogViewSet, basename='log')
+
+# urlpatterns = [    
+#     path('', include(router.urls)),  
+# ]
+
+urlpatterns = router.urls
 
--- logs/views.py
+++ logs/views.py
@@ -19,85 +19,15 @@ from asdt_api.authentication import ASDTAuthentication
 from bson.objectid import ObjectId
 from .models import *
 from user.models import *
+from asdt_api.utils import get_logger
 
-class LogByIdView(APIView):
-    authentication_classes = [ASDTAuthentication]
-    permission_classes = (IsAuthenticated,)
+logger = get_logger()
 
-    def get_allowed(self, user, queryset):
-        # Allowed detector list for user
-        detector_set_for_user = set()
-        for detector in user.group.devices.detectors:
-            detector_set_for_user.add( str(detector.fetch().id) )
+class LogViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
 
-        # Check logs allowed
-        log_allowed = []
-        for log in queryset:
-            detector_set = set()
-            for detector in log.detectors:
-                detector_set.add(str(detector.id))
-            if len( detector_set & detector_set_for_user ) > 0:
-                log_allowed.append(log['id'])
-        return log_allowed
-
-    def get(self, request, log_id = None):
-
-        # Get queryset
-        queryset = Log.objects.filter(id=log_id)
-        if len(queryset) != 1:
-            return Response({"success": False, "error": "NOT_FOUND"})
-
-
-        # # Apply filtering            
-        # log_allowed = self.get_allowed(request.user, queryset)
-        # queryset = queryset.filter(id__in=log_allowed)
-        # if queryset.count() == 0:
-        #     return Response({"success": False, "error": "NOT_ALLOWED"})
-
-
-        # Prepare results
-        pipeline = [
-            {
-                "$project": {
-                    #"_id": {  "$toString": "$_id" },
-                    "dateIni": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S", "date": "$dateIni" } },
-                    "dateFin": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S", "date": "$dateIni" } },
-                    "productId": { "$ifNull": [ "$productId", "undef" ] },
-                    "sn": { "$ifNull": [ "$sn", "undef" ] },
-                    "model": { "$ifNull": [ "$model", "undef" ] },
-                    "owner": { "$ifNull": [ "$owner", "undef" ] },
-                    "maxHeight": { "$ifNull": [ "$maxHeight", "undef" ] },
-                    "distanceTraveled": { "$ifNull": [ "$distanceTraveled", "undef" ] },
-                    "distanceToDetector": { "$ifNull": [ "$distanceToDetector", "undef" ] },
-                    "driverLocation": { "$ifNull": [ "$driverLocation", "undef" ] },
-                    "homeLocation": { "$ifNull": [ "$homeLocation", "undef" ] },
-                    #"id": {  "$toString": "$_id" },
-                    "detectors" : "$detectors",
-                    "route": { "lat": 1, "lon": 1, "aHeight": 1, "fHeight": 1, "time": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S:%L", "date": "$dateIni" } }  },
-                }
-            }
-        ]
-
-        # Iterate cursor
-        cursor = queryset.aggregate(*pipeline)
-        data = cursor.next()
-
-        # NOTE: This needs to be improved
-        data['_id'] = str(data['_id'])
-        data['id'] = str(data['_id'])
-        detectors_list = []
-        for id in data['detectors']:
-            detectors_list.append(str(id))
-        data['detectors'] = detectors_list        
-
-        data = { 
-            'success': True,
-            'data': data
-        } 
-        return Response(data)
-
-
-class LogByPageView(APIView):
     authentication_classes = [ASDTAuthentication]
     permission_classes = (IsAuthenticated,)
 
@@ -130,7 +60,7 @@ class LogByPageView(APIView):
                 log_allowed.append(log['id'])
         return log_allowed
 
-    def get(self, request):
+    def list(self, request):
 
         # Get dateIni / dateFin / sn / page
         dateIni = datetime.datetime.now() - timedelta(days=4)
@@ -139,12 +69,8 @@ class LogByPageView(APIView):
         dateFin = datetime.datetime.now()
         if 'dateFin' in request.query_params:
             dateFin = datetime.datetime.strptime(request.query_params['dateFin'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        sn = None
-        if 'sn' in request.query_params:
-            sn = request.query_params['sn']
-        page = 0
-        if 'page' in request.query_params:
-            page = int(request.query_params['page'])
+        sn = self.request.query_params.get('sn', None)
+        page = int(self.request.query_params.get('page', 0))
 
         # Select query to apply
         query = { "$and": [ {"dateIni": {"$gt": dateIni }}, {"dateFin": {"$lte": dateFin }}] }
@@ -209,11 +135,62 @@ class LogByPageView(APIView):
          } 
         return Response(data)
 
-# class GetFlightAll(APIView):
-#     authentication_classes = [ASDTAuthentication]
-#     permission_classes = (IsAuthenticated,)
+    def retrieve(self, request, pk=None):
+        print("pk", pk)
+        # Get queryset
+        queryset = Log.objects.filter(id=pk)
+        if len(queryset) != 1:
+            logger.info("Retreived: " + str(len(queryset)))
+            return Response({"success": False, "error": "NOT_FOUND"})
 
-#     def post(self, request):
-#         print(request.user.data)
-#         data = { 'success -all': True } 
-#         return Response(data)        
+        # Apply filtering            
+        log_allowed = self.get_allowed(request.user, queryset)
+        queryset = queryset.filter(id__in=log_allowed)
+        if queryset.count() == 0:
+            return Response({"success": False, "error": "NOT_ALLOWED"})
+
+        # Prepare results
+        pipeline = [
+            {
+                "$project": {
+                    #"_id": {  "$toString": "$_id" },
+                    "dateIni": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S", "date": "$dateIni" } },
+                    "dateFin": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S", "date": "$dateIni" } },
+                    "productId": { "$ifNull": [ "$productId", "undef" ] },
+                    "sn": { "$ifNull": [ "$sn", "undef" ] },
+                    "model": { "$ifNull": [ "$model", "undef" ] },
+                    "owner": { "$ifNull": [ "$owner", "undef" ] },
+                    "maxHeight": { "$ifNull": [ "$maxHeight", "undef" ] },
+                    "distanceTraveled": { "$ifNull": [ "$distanceTraveled", "undef" ] },
+                    "distanceToDetector": { "$ifNull": [ "$distanceToDetector", "undef" ] },
+                    "driverLocation": { "$ifNull": [ "$driverLocation", "undef" ] },
+                    "homeLocation": { "$ifNull": [ "$homeLocation", "undef" ] },
+                    #"id": {  "$toString": "$_id" },
+                    "detectors" : "$detectors",
+                    "route": { "lat": 1, "lon": 1, "aHeight": 1, "fHeight": 1, "time": { "$dateToString": { "format": "%Y-%m-%dT%H:%M:%S:%L", "date": "$dateIni" } }  },
+                }
+            }
+        ]
+
+        # Iterate cursor
+        cursor = queryset.aggregate(*pipeline)
+        #data = cursor.next()
+
+        data = []
+        for item in cursor:
+            item['_id'] = str(item['_id'])
+            item['id'] = str(item['_id'])
+            detectors_list = []
+            for id in item['detectors']:
+                detectors_list.append(str(id))
+            item['detectors'] = detectors_list
+            data.append(item)
+        data_single = data[0]
+
+        data = { 
+            'success': True,
+            'data': data_single
+        } 
+        return Response(data)
+
+ 
