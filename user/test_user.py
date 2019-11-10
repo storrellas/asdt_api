a84a35a6ad677204430780effedda76e7ed343da
storrellas <storrellas@gmail.com>
2019-11-10 21:02:18 +0000

    ENH: Fixed all test cases

--- user/test_user.py
+++ user/test_user.py
@@ -113,87 +113,95 @@ class TestCase(APITestCase):
     response_json = json.loads(response.content.decode())
     self.assertTrue(response_json['success'])
 
-    # Get Token
-    self.authenticate("user@asdt.eu", "asdt2019")
+    # Get token
+    self.client.credentials(HTTP_AUTHORIZATION='')
+    response = self.client.post('/{}/user/authenticate/'.format(settings.PREFIX), 
+                                { "email": "user@test.eu", "password": "asdt2019" })
+    self.assertTrue(response.status_code == HTTPStatus.OK)
+    response_json = json.loads(response.content.decode())
+    self.assertTrue(response_json['success'])
 
     # Delete created user
     User.objects.filter(email='user@test.eu').delete()
 
-  # def test_create_user_group(self):
+  def test_create_user_group(self):
     
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add user to group
-  #   group = Group.objects.get(name='ADMIN_CHILD_ASDT')
+    # Add user to group
+    group = Group.objects.get(name='ADMIN_CHILD_ASDT')
 
-  #   # Check not workin without login
-  #   body = {
-  #     "email": "user2@test.eu",
-  #     "password": "asdt2019",
-  #     "name": "Oussama",
-  #     "role": "EMPOWERED",
-  #     "hasGroup": True,
-  #     "group": group.id
-  #   }
-  #   response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
-  #   response_json = json.loads(response.content.decode())
-  #   self.assertTrue(response_json['success'])
-  #   self.assertTrue(response_json['data']['group'] == str(group.id))
+    # Check not workin without login
+    body = {
+      "email": "user2@test.eu",
+      "password": "asdt2019",
+      "name": "Oussama",
+      "role": "EMPOWERED",
+      "hasGroup": True,
+      "group": group.id
+    }
+    response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
+    self.assertTrue(response.status_code == HTTPStatus.OK)
+    response_json = json.loads(response.content.decode())
+    self.assertTrue(response_json['success'])
+    self.assertTrue(response_json['data']['group'] == str(group.id))
 
-  #   # Get Token
-  #   self.authenticate("user2@asdt.eu", "asdt2019")
+    # Get token
+    self.client.credentials(HTTP_AUTHORIZATION='')
+    response = self.client.post('/{}/user/authenticate/'.format(settings.PREFIX), 
+                                { "email": "user2@test.eu", "password": "asdt2019" })
+    self.assertTrue(response.status_code == HTTPStatus.OK)
+    response_json = json.loads(response.content.decode())
+    self.assertTrue(response_json['success'])
 
-  #   # # Delete created user
-  #   # User.objects.filter(email='user2@test.eu').delete()
-  #   # Delete user
-  #   user = User.objects.get(email='user2@test.eu')
-  #   group = Group.objects.get(name='ADMIN_CHILD_ASDT')    
-  #   group.users.remove(user)
-  #   group.save()
+    # Delete user
+    user = User.objects.get(email='user2@test.eu')
+    group = Group.objects.get(name='ADMIN_CHILD_ASDT')    
+    group.users.remove(user)
+    group.save()
 
-  #   # Delete user
-  #   user.delete()
+    # Delete user
+    user.delete()
 
 
-  # def test_create_user_forbidden(self):
+  def test_create_user_forbidden(self):
     
-  #   # Get Token
-  #   self.authenticate("viewer@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("viewer@asdt.eu", "asdt2019")
 
-  #   # Check not workin without login
-  #   body = {
-  #     "email": "user@test.com",
-  #     "password": "asdt2019",
-  #     "name": "Oussama",
-  #     "role": "EMPOWERED",
-  #     "hasGroup": False
-  #   }
-  #   response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
-  #   self.assertTrue(response.status_code == HTTPStatus.FORBIDDEN)
+    # Check not workin without login
+    body = {
+      "email": "user@test.com",
+      "password": "asdt2019",
+      "name": "Oussama",
+      "role": "EMPOWERED",
+      "hasGroup": False
+    }
+    response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
+    self.assertTrue(response.status_code == HTTPStatus.FORBIDDEN)
 
-  # def test_create_user_group_not_allowed(self):
+  def test_create_user_group_not_allowed(self):
     
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add user to group
-  #   group = Group.objects.get(name='VIEWER_ASDT')
+    # Add user to group
+    group = Group.objects.get(name='VIEWER_ASDT')
 
-  #   # Check not workin without login
-  #   body = {
-  #     "email": "user@test.com",
-  #     "password": "asdt2019",
-  #     "name": "Oussama",
-  #     "role": "EMPOWERED",
-  #     "hasGroup": True,
-  #     "group": group.id
-  #   }
-  #   response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
-  #   response_json = json.loads(response.content.decode())
-  #   self.assertFalse(response_json['success'])
+    # Check not workin without login
+    body = {
+      "email": "user@test.com",
+      "password": "asdt2019",
+      "name": "Oussama",
+      "role": "EMPOWERED",
+      "hasGroup": True,
+      "group": group.id
+    }
+    response = self.client.post('/{}/user/'.format(settings.PREFIX), body)
+    self.assertTrue(response.status_code == HTTPStatus.OK)
+    response_json = json.loads(response.content.decode())
+    self.assertFalse(response_json['success'])
 
 
   def test_list_admin(self):
