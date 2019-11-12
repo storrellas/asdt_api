a7ae9855e3416ff2e818d89d68af8642abda715c
storrellas <storrellas@gmail.com>
2019-11-12 14:34:57 +0000

    ENH: First steps with detector_sim

--- tools/detector_sim/detector_sim.py
+++ tools/detector_sim/detector_sim.py
@@ -1,7 +1,38 @@
-from utils import get_logger
+import requests
+import json
+import sys
+from http import HTTPStatus
 
+# Project imports
+from utils import get_logger
 
 logger = get_logger()
 
+# Configuration variables
+host = 'http://asdtdev.mooo.com'
+detector = '5db1b05fedd685190719f924'
+password = 'test'
+PORT = 80
+
+# Session variables
+token = None
+
+def login(id, password):
+  body = { 'id': id, 'password': password}
+  response = requests.post(host + '/detector/authenticate', data=body)
+  # Get token
+  if response.status_code == HTTPStatus.OK:
+    response_json = json.loads(response.content.decode()) 
+    return response_json['data']['token']
+  else:
+    return ''
+  
+  print(response_json)
+
 if __name__ == "__main__":
-  logger.info("MyDetectorSim")
+  logger.info("Login detector with {} / {} ".format(detector, password))
+  token = login(detector, password)
+  if len(token) == 0:
+    logger.error("Login not successful. Aborting")
+    sys.exit(0)
+  
