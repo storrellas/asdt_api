5db4b9323008231df6a8bf91b7707726597f603a
storrellas <storrellas@gmail.com>
2019-11-12 14:45:29 +0000

    ENH: More steps towards websocket client

--- tools/detector_sim/detector_sim.py
+++ tools/detector_sim/detector_sim.py
@@ -29,6 +29,49 @@ def login(id, password):
   
   print(response_json)
 
+##############################
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import gen
+from tornado.websocket import websocket_connect
+class Client(object):
+  def __init__(self, url, timeout):
+    self.url = url
+    self.timeout = timeout
+    self.ioloop = IOLoop.instance()
+    self.ws = None
+    self.connect()
+    PeriodicCallback(self.keep_alive, 20000).start()
+    self.ioloop.start()
+
+  @gen.coroutine
+  def connect(self):
+    print("trying to connect")
+    try:
+      self.ws = yield websocket_connect(self.url)
+      self.ws.write_message( token )
+    except Exception as e:
+      print("connection error")
+    else:
+      print("connected")
+      self.run()
+
+  @gen.coroutine
+  def run(self):
+    while True:
+      msg = yield self.ws.read_message()
+      print("msg", msg)
+      if msg is None:
+        print("connection closed")
+        self.ws = None
+        break
+
+  def keep_alive(self):
+    if self.ws is None:
+      self.connect()
+    else:
+      self.ws.write_message("keep alive")
+##############################
+
 if __name__ == "__main__":
   logger.info("Login detector with {} / {} ".format(detector, password))
   token = login(detector, password)
@@ -36,3 +79,8 @@ if __name__ == "__main__":
     logger.error("Login not successful. Aborting")
     sys.exit(0)
   
+  # Create websocket client
+  client = Client("ws://asdtdev.mooo.com/api", 5)
+
+
+
