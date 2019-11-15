a4f16919d547966c8fa2f9df49a887b2791c201e
storrellas <storrellas@gmail.com>
2019-11-15 20:21:00 +0000

    ENH: Add/Remove device from group comes to life again

--- groups/test_devices.py
+++ groups/test_devices.py
@@ -38,157 +38,157 @@ class TestCase(helper_tests.ASDTTestCase):
     """
     pass
   
-  # def test_add_delete_detector(self):
+  def test_add_delete_detector(self):
     
-  #   detector = Detector.objects.get(name='detector4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
+    detector = Detector.objects.get(name='detector4')
+    group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add inhibitor to group
-  #   response = self.client.post('/{}/groups/{}/devices/detectors/{}/'.format(settings.PREFIX, group.id, detector.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Add inhibitor to group
+    response = self.client.post('/{}/groups/{}/devices/detectors/{}/'.format(settings.PREFIX, group.id, detector.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  #   # Check operation
-  #   detector = Detector.objects.get(name='detector4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertTrue( detector in group.devices.detectors )
-  #   self.assertTrue( group in detector.groups )
+    # Check operation
+    detector = Detector.objects.get(name='detector4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertTrue( detector in group.devices.detectors )
+    self.assertTrue( group in detector.groups )
 
-  #   # Remove inhibitor from group
-  #   response = self.client.delete('/{}/groups/{}/devices/detectors/{}/'.format(settings.PREFIX, group.id, detector.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
+    # Remove inhibitor from group
+    response = self.client.delete('/{}/groups/{}/devices/detectors/{}/'.format(settings.PREFIX, group.id, detector.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
 
-  #   # Check operation
-  #   detector = Detector.objects.get(name='detector4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertFalse( detector in group.devices.detectors )
-  #   self.assertFalse( group in detector.groups )
+    # Check operation
+    detector = Detector.objects.get(name='detector4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertFalse( detector in group.devices.detectors )
+    self.assertFalse( group in detector.groups )
 
-  # def test_add_delete_drone(self):
+  def test_add_delete_drone(self):
     
-  #   drone = Drone.objects.get(sn='4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
+    drone = Drone.objects.get(sn='4')
+    group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add inhibitor to group
-  #   response = self.client.post('/{}/groups/{}/devices/drones/{}/'.format(settings.PREFIX, group.id, drone.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Add inhibitor to group
+    response = self.client.post('/{}/groups/{}/devices/drones/{}/'.format(settings.PREFIX, group.id, drone.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  #   # Check operation
-  #   drone = Drone.objects.get(sn='4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertTrue( drone in group.devices.friendDrones )
-  #   self.assertTrue( group in drone.groups )
+    # Check operation
+    drone = Drone.objects.get(sn='4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertTrue( drone in group.devices.friendDrones )
+    self.assertTrue( group in drone.groups )
 
-  #   # Remove inhibitor from group
-  #   response = self.client.delete('/{}/groups/{}/devices/drones/{}/'.format(settings.PREFIX, group.id, drone.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
+    # Remove inhibitor from group
+    response = self.client.delete('/{}/groups/{}/devices/drones/{}/'.format(settings.PREFIX, group.id, drone.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
 
-  #   # Check operation
-  #   drone = Drone.objects.get(sn='4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertFalse( drone in group.devices.friendDrones )
-  #   self.assertFalse( group in drone.groups )
+    # Check operation
+    drone = Drone.objects.get(sn='4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertFalse( drone in group.devices.friendDrones )
+    self.assertFalse( group in drone.groups )
 
-  # def test_add_delete_inhibitor(self):
+  def test_add_delete_inhibitor(self):
     
-  #   inhibitor = Inhibitor.objects.get(name='inhibitor4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
+    inhibitor = Inhibitor.objects.get(name='inhibitor4')
+    group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add inhibitor to group
-  #   response = self.client.post('/{}/groups/{}/devices/inhibitors/{}/'.format(settings.PREFIX, group.id, inhibitor.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Add inhibitor to group
+    response = self.client.post('/{}/groups/{}/devices/inhibitors/{}/'.format(settings.PREFIX, group.id, inhibitor.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  #   # Check operation
-  #   inhibitor = Inhibitor.objects.get(name='inhibitor4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertTrue( inhibitor in group.devices.inhibitors )
-  #   self.assertTrue( group in inhibitor.groups )
+    # Check operation
+    inhibitor = Inhibitor.objects.get(name='inhibitor4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertTrue( inhibitor in group.devices.inhibitors )
+    self.assertTrue( group in inhibitor.groups )
 
-  #   # Remove inhibitor from group
-  #   response = self.client.delete('/{}/groups/{}/devices/inhibitors/{}/'.format(settings.PREFIX, group.id, inhibitor.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
+    # Remove inhibitor from group
+    response = self.client.delete('/{}/groups/{}/devices/inhibitors/{}/'.format(settings.PREFIX, group.id, inhibitor.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
 
-  #   # Check operation
-  #   inhibitor = Inhibitor.objects.get(name='inhibitor4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertFalse( inhibitor in group.devices.inhibitors )
-  #   self.assertFalse( group in inhibitor.groups )
+    # Check operation
+    inhibitor = Inhibitor.objects.get(name='inhibitor4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertFalse( inhibitor in group.devices.inhibitors )
+    self.assertFalse( group in inhibitor.groups )
 
-  # def test_add_delete_zone(self):
+  def test_add_delete_zone(self):
     
-  #   zone = Zone.objects.get(name='zone4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
+    zone = Zone.objects.get(name='zone4')
+    group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Add inhibitor to group
-  #   response = self.client.post('/{}/groups/{}/devices/zones/{}/'.format(settings.PREFIX, group.id, zone.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Add inhibitor to group
+    response = self.client.post('/{}/groups/{}/devices/zones/{}/'.format(settings.PREFIX, group.id, zone.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  #   # Check operation
-  #   zone = Zone.objects.get(name='zone4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertTrue( zone in group.devices.zones )
-  #   self.assertTrue( group in zone.groups )
+    # Check operation
+    zone = Zone.objects.get(name='zone4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertTrue( zone in group.devices.zones )
+    self.assertTrue( group in zone.groups )
 
-  #   # Remove inhibitor from group
-  #   response = self.client.delete('/{}/groups/{}/devices/zones/{}/'.format(settings.PREFIX, group.id, zone.id), {})
-  #   self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
+    # Remove inhibitor from group
+    response = self.client.delete('/{}/groups/{}/devices/zones/{}/'.format(settings.PREFIX, group.id, zone.id), {})
+    self.assertTrue(response.status_code == HTTPStatus.NO_CONTENT)
 
-  #   # Check operation
-  #   zone = Zone.objects.get(name='zone4')
-  #   group = Group.objects.get(name='ADMIN_ASDT')
-  #   self.assertFalse( zone in group.devices.zones )
-  #   self.assertFalse( group in zone.groups )
+    # Check operation
+    zone = Zone.objects.get(name='zone4')
+    group = Group.objects.get(name='ADMIN_ASDT')
+    self.assertFalse( zone in group.devices.zones )
+    self.assertFalse( group in zone.groups )
 
-  # def test_get_group_drones(self):
-  #   admin_group = Group.objects.get(name='ADMIN_ASDT')
+  def test_get_group_drones(self):
+    admin_group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Request drones
-  #   response = self.client.get('/{}/groups/{}/drones/'.format(settings.PREFIX, admin_group.id))
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Request drones
+    response = self.client.get('/{}/groups/{}/drones/'.format(settings.PREFIX, admin_group.id))
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  # def test_get_group_detectors(self):
-  #   admin_group = Group.objects.get(name='ADMIN_ASDT')
+  def test_get_group_detectors(self):
+    admin_group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Request detectors
-  #   response = self.client.get('/{}/groups/{}/devices/detectors/'.format(settings.PREFIX, admin_group.id))
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Request detectors
+    response = self.client.get('/{}/groups/{}/devices/detectors/'.format(settings.PREFIX, admin_group.id))
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  # def test_get_group_inhibitors(self):
-  #   admin_group = Group.objects.get(name='ADMIN_ASDT')
+  def test_get_group_inhibitors(self):
+    admin_group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Request inhibitors
-  #   response = self.client.get('/{}/groups/{}/devices/inhibitors/'.format(settings.PREFIX, admin_group.id))
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Request inhibitors
+    response = self.client.get('/{}/groups/{}/devices/inhibitors/'.format(settings.PREFIX, admin_group.id))
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
-  # def test_get_group_zones(self):
-  #   admin_group = Group.objects.get(name='ADMIN_ASDT')
+  def test_get_group_zones(self):
+    admin_group = Group.objects.get(name='ADMIN_ASDT')
 
-  #   # Get Token
-  #   self.authenticate("admin@asdt.eu", "asdt2019")
+    # Get Token
+    self.authenticate("admin@asdt.eu", "asdt2019")
 
-  #   # Request zones
-  #   response = self.client.get('/{}/groups/{}/devices/zones/'.format(settings.PREFIX, admin_group.id))
-  #   self.assertTrue(response.status_code == HTTPStatus.OK)
+    # Request zones
+    response = self.client.get('/{}/groups/{}/devices/zones/'.format(settings.PREFIX, admin_group.id))
+    self.assertTrue(response.status_code == HTTPStatus.OK)
 
   def get_group_devices(self, url):
     
